a3923467fc113111d411d35df728bc7939d63b30
Wessel van Ree <44018037+wesselvanree@users.noreply.github.com>
2025-04-30 09:30:51 +0200

    create content config and define blog collection

---
+++ app/blog/page.tsx
@@ -0,0 +1,28 @@
+import { collections } from '@/content.config'
+import Link from 'next/link'
+
+export default async function BlogPage() {
+  const articles = collections.blog.getAll()
+
+  return (
+    <div className="py-12 px-4">
+      <div className="max-w-xl mx-auto">
+        <h1 className="text-3xl font-semibold mb-4">Blog</h1>
+        {articles.map((article) => (
+          <div key={article.slug} className="isolate relative z-0">
+            <img src={article.image} alt={article.imageAlt ?? ''} />
+            <h2 className="text-xl font-semibold">
+              <Link href={`/blog/${article.slug}`}>
+                <span>{article.title}</span>
+                <span className="absolute left-0 top-0 size-full"></span>
+              </Link>
+            </h2>
+            <div className="text-gray-500 text-sm">
+              {article.publishedAt.toLocaleDateString('en-us', { dateStyle: 'long' })}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
---
+++ content.config.ts
@@ -0,0 +1,17 @@
+import { z } from 'zod'
+import { defineCollection } from './lib/content'
+
+const blog = defineCollection({
+  dir: './content/blog',
+  schema: z.object({
+    title: z.string(),
+    publishedAt: z.coerce.date(),
+    image: z.string().optional(),
+    imageAlt: z.string().optional(),
+    tags: z.array(z.string()).optional(),
+  }),
+})
+
+export const collections = {
+  blog,
+}
