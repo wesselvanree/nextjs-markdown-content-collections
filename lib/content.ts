2f235becaccca5416f04fc3fe0237bac3a8afd8f
Wessel van Ree <44018037+wesselvanree@users.noreply.github.com>
2025-04-30 09:30:34 +0200

    create defineCollection function

---
+++ lib/content.ts
@@ -0,0 +1,74 @@
+import fs from 'fs'
+import matter from 'gray-matter'
+import { join } from 'path'
+import { z } from 'zod'
+
+import 'server-only'
+
+type ContentCollection<S> = {
+  getSlugs: () => string[]
+  getEntryBySlug: (slug: string) =>
+    | (S & {
+        slug: string
+        content: string
+      })
+    | null
+  getAll: () => (S & {
+    slug: string
+    content: string
+  })[]
+}
+
+export function defineCollection<S extends Record<string, unknown>>(config: {
+  dir: string
+  schema: z.ZodType<S>
+}) {
+  const collectionDirectory = join(process.cwd(), config.dir)
+
+  function getSlugs() {
+    return fs
+      .readdirSync(collectionDirectory)
+      .filter((file) => file.endsWith('.md'))
+      .map((entry) => entry.replace(/\.md$/, ''))
+  }
+
+  function getEntryBySlug(slug: string) {
+    const fullPath = join(collectionDirectory, `${slug}.md`)
+
+    if (!fs.existsSync(fullPath)) {
+      return null
+    }
+
+    const fileContents = fs.readFileSync(fullPath, 'utf8')
+    const { data, content } = matter(fileContents)
+
+    const validated = config.schema.safeParse(data)
+
+    if (!validated.success) {
+      throw new Error(
+        `Invalid frontmatter in ${config.dir}/${slug}: \n${JSON.stringify(
+          validated.error.flatten().fieldErrors,
+          undefined,
+          4
+        )}`
+      )
+    }
+
+    return { ...validated.data, slug, content }
+  }
+
+  function getAll() {
+    const slugs = getSlugs()
+    const posts = slugs.map((slug) => getEntryBySlug(slug)!)
+
+    return posts
+  }
+
+  const result: ContentCollection<S> = {
+    getSlugs,
+    getEntryBySlug,
+    getAll,
+  }
+
+  return result
+}
